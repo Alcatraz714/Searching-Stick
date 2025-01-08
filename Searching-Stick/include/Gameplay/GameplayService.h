7eba973fb02665da7d4f24db985bad0f19343c9c
Rishi Saxena <rishi.saxena@gamestacy.com>
2025-01-08 14:04:30 +0530

    Added Stick and Collection - MVC

--- Searching-Stick/include/Gameplay/GameplayService.h
+++ Searching-Stick/include/Gameplay/GameplayService.h
@@ -1,25 +1,33 @@
 #pragma once
 #include <SFML/System/String.hpp>
+#include "Gameplay/StickCollection/StickCollectionModel.h"
+#include "Gameplay/StickCollection/StickCollectionController.h"
 
 namespace Gameplay
 {
+	using namespace Collection;
 	class GameplayController;
+	enum class SearchType;
 
 	class GameplayService
 	{
 	private:
 		GameplayController* gameplay_controller;
+		StickCollectionContoller* collection_controller;
 
 	public:
 		GameplayService();
 		~GameplayService();
-
-		// Lifecycle methods
+		// Lifecycle Methods
 		void initialize();
 		void update();
 		void render();
 
 		// Functions
 		void reset();
+		void searchElement(Collection::SearchType search_type);
+		// Stick
+		Collection::SearchType getCurrentSearchType();
+		int getNumberOfSticks();
 	};
 }
